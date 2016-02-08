e750bf0433342fcf93c999523fc90197b4b6c5e0
Dhruv <dhruv.10000@gmail.com>
2016-02-08 20:01:26 +0530

    commit

--- sql.py
+++ sql.py
@@ -1,14 +1,9 @@
 import sqlite3
 
-with sqlite3.connect("shows.db") as connection:
+with sqlite3.connect("imdb.db") as connection:
 #creates database if it does not exist
 	c = connection.cursor()
-	c.execute('CREATE TABLE show(title TEXT , description TEXT) ')
-	# c.execute('INSERT INTO posts VALUES("Good" , "I\'m good")')
-	# c.execute('INSERT INTO posts VALUES("Well" , "I\'m well")')
-
-	# c.execute('CREATE TABLE IF NOT EXISTS codes(id INTEGER , code TEXT) ')
-	# codeString = 'print "Hello world" '
-	# c.execute("INSERT INTO codes VALUES (1, ?)", (codeString,))
-	# codeString = 'print "Second code" '
-	# c.execute("INSERT INTO codes VALUES (2, ?)", (codeString,))
+	# c.execute('DROP TABLE show')
+	# c.execute('DROP TABLE series')
+	c.execute('CREATE TABLE IF NOT EXISTS movies(title TEXT , rating FLOAT(1) ) ')
+	c.execute('CREATE TABLE IF NOT EXISTS series(title TEXT , rating FLOAT(1) ) ')
